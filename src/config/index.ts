6edb7d241ae4a8fdb9329670eb24f149cb0d20c5
Jack <kitty2021tyj@gmail.com>
2024-12-02 13:29:41 +0800

    ✨ feat:新增其他模板类型

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,10 +9,25 @@ export const REGISTER = {
 export { name, version } from '../../package.json'
 
 export const templates = {
+  // 后台管理系统
   admin: {
     GITEE_URL: '', // gitee模板下载地址
     GITHUB_URL: '', // github模板下载地址
-    DESC: 'vue-admin 完整版本', // 模板描述
+    DESC: 'quick-admin 完整版本', // 模板描述
     BRANCH: 'master' // 分支
   },
+  // H5
+  h5: {
+    GITEE_URL: '', // gitee模板下载地址
+    GITHUB_URL: '', // github模板下载地址
+    DESC: 'quick-h5 完整版本', // 模板描述
+    BRANCH: 'master' // 分支
+  },
+  // mini-program
+  minProgram: {
+    GITEE_URL: '', // gitee模板下载地址
+    GITHUB_URL: '', // github模板下载地址
+    DESC: 'quick-minProgram 完整版本', // 模板描述
+    BRANCH: 'master' // 分支
+  }
 }
