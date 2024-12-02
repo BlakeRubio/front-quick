a6ec2422d658932f62b78ccc89d552171a238801
Jack <kitty2021tyj@gmail.com>
2024-12-02 11:04:16 +0800

    ✨ feat:新增脚手架帮助命令

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { cac } from 'cac'
+import figlet from 'figlet'
 import { inputProjectName, chooseDownloadOrigin } from './prompt'
 import { create } from './template'
 import { version, templates } from './config'
-// import { isExistsFile } from './has-file'
 import { ProjectTemplate } from './types'
 import { clg } from './utils'
 
@@ -17,7 +17,7 @@ cli
   .option('-f, --force', '如果目标文件存在，则强制覆盖')
   // 指定项目从 github 下载
   .option('-g --github', '使用github模板地址')
-  .action(async (cmd) => {
+  .action(async () => {
     const projectName = await inputProjectName()
     const isDownloadForGithub = await chooseDownloadOrigin()
     create(projectName, undefined, isDownloadForGithub)
@@ -28,6 +28,20 @@ cli.command('list', '查看所有模板').action(async () => {
   Object.keys(templates).forEach((key: string) => {
     clg(`${key} ${templates[key as ProjectTemplate].DESC}`)
   })
+})
+
+// 查看脚手架帮助
+cli.help(() => {
+  clg(
+    '\r\n' +
+      figlet.textSync('quick', {
+        font: '3D-ASCII',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 80,
+        whitespaceBreak: true
+      })
+  )
 })
 
 cli.parse()
